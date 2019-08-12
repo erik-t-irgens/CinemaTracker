6d4237294d71c05540312c361aa7f1d97d4567e1
Emerson Jordan and Erik Irgens <emerson@>
2019-08-12 16:44:21 -0700

    Add a few views and finalize Models and Controllers. Stopped on Delete.cshtml, needs updating.

--- CinemaTracker/Controllers/HomeController.cs
+++ CinemaTracker/Controllers/HomeController.cs
@@ -9,11 +9,23 @@ namespace CinemaTracker.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly CinemaTrackerContext _db;
+        public HomeController(CinemaTrackerContext db)
+        {
+            _db = db;
+        }
+
         [HttpGet("/")]
         //does that matter?
         public ActionResult Index()
         {
-            return View();
+            var model = _db.Genres.
+            Include(genre => genre.Movies)
+            .ThenInclude(join => join.Movie)
+            .ToList();
+            return View(model);
+
+
         }
     }
 }
